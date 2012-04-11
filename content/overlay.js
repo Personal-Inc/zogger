d1c04fcef50283cad727e10e30b7082c52e08c3a
Tarik Kurspahic <tarik@personal.com>
2012-04-11 16:56:26 -0400

    trapping URIs

--- content/overlay.js
+++ content/overlay.js
@@ -5,9 +5,35 @@ var Zogger = {
   },
 
   onMenuItemCommand: function() {
-	logObject2Console(console);
     gBrowser.loadOneTab("chrome://zogger/content/zogger-report.xul");
   }
 };
 
+var httpRequestObserver =  
+{  
+  observe: function(subject, topic, data)  
+  {  
+    if (topic == "http-on-modify-request") {  
+      var httpChannel = subject.QueryInterface(Components.interfaces.nsIHttpChannel);  
+      logObject2Console(httpChannel.originalURI); 
+    }  
+  },  
+  
+  get observerService() {  
+    return Components.classes["@mozilla.org/observer-service;1"]  
+                     .getService(Components.interfaces.nsIObserverService);  
+  },  
+  
+  register: function()  
+  {  
+    this.observerService.addObserver(this, "http-on-modify-request", false);  
+  },  
+  
+  unregister: function()  
+  {  
+    this.observerService.removeObserver(this, "http-on-modify-request");  
+  }  
+};
+
 window.addEventListener("load", function(e) { Zogger.onLoad(e); }, false); 
+httpRequestObserver.register();
