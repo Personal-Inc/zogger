2962e0fc404cd212bbe9d61640996978ae685537
vedadk <vedad@personal.com>
2012-04-11 11:24:03 -0400

    Install script : extracted extension id

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
+EXTENSION_ID=zogger@personal.com
 LOCATION=`pwd`
 PROFILES="/Users/$(whoami)/Library/Application Support/Firefox/Profiles/"
 DEV_PROFILE=`ls "$PROFILES" | grep .dev`
 mkdir -p "$PROFILES/$DEV_PROFILE/extensions/" 
-echo $LOCATION > "$PROFILES/$DEV_PROFILE/extensions/zogger@personal.com" 
+echo $LOCATION > "$PROFILES/$DEV_PROFILE/extensions/$EXTENSION_ID" 
