0944aae5764dfd9ad398978f70b3a705b74ab1ec
vedadk <vedad@personal.com>
2012-04-13 10:41:00 -0400

    Collecting form data into collector

--- data/submits-handler.js
+++ data/submits-handler.js
@@ -1,21 +1,21 @@
 jQuery(function (){
 
     $('form').submit(function(){
+        var submitData = { data : {}};
         $(this).find("input").each(function(index, element)
             {
-                console.log("--------------------");
-
                 var fieldName = element.name;
                 var fieldValue = $(element).val();
 
                 if((/pass|password|pwd/).test(fieldName))
                     return;
 
-                console.log(fieldName);
-                console.log(fieldValue);
-                //collector.collectData(fieldName, fieldValue);
-                console.log("--------------------");
+                submitData.data[fieldName] = fieldValue; 
             }
         )
+        console.log("--------------------");
+        console.log(submitData);
+        console.log("--------------------");
+        self.port.emit('collectRequest', submitData);
     })
 })
