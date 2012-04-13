875db17b0b386eb244d0d39e87ac9d82e625263e
vedadk <vedad@personal.com>
2012-04-13 12:58:25 -0400

    Dummy data for Bruno

--- data/ui/report.js
+++ data/ui/report.js
@@ -1,3 +1,25 @@
+var reportData = [ {"type":"httpResponse","domain":"mozilla.org","referrerDomain":"mozilla.org","contentType":"text/css"},
+{"type":"httpResponse","domain":"mozilla.org","referrerDomain":"mozilla.org","contentType":"image/png"},
+{"type":"httpResponse","domain":"mozilla.org","referrerDomain":"mozilla.org","contentType":"text/javascript"},
+{"type":"httpResponse","domain":"mozilla.org","referrerDomain":"mozilla.org","contentType":"text/javascript"},
+{"type":"httpResponse","domain":"mozilla.org","referrerDomain":"mozilla.org","contentType":"text/javascript"},
+{"type":"httpResponse","domain":"mozilla.org","referrerDomain":"mozilla.org","contentType":"text/css"},
+{"type":"httpResponse","domain":"mozilla.org","referrerDomain":"mozilla.org","contentType":"text/css"},
+{"type":"httpResponse","domain":"mozilla.org","referrerDomain":"mozilla.org","contentType":"text/javascript"},
+{"type":"httpResponse","domain":"mozilla.org","referrerDomain":"mozilla.org","contentType":"text/javascript"},
+{"type":"httpResponse","domain":"mozilla.org","referrerDomain":"mozilla.org","contentType":"image/png"},
+{"type":"httpResponse","domain":"mozilla.org","referrerDomain":"mozilla.org","contentType":"image/png"},
+{"type":"httpResponse","domain":"webtrendslive.com","referrerDomain":"mozilla.org","contentType":"application/x-javascript"},
+{"type":"httpResponse","domain":"mozilla.org","referrerDomain":"mozilla.org","contentType":"image/png"},
+{"type":"httpResponse","domain":"mozilla.org","referrerDomain":"mozilla.org","contentType":"text/plain; charset=UTF-8"},
+{"type":"httpResponse","domain":"mozilla.org","referrerDomain":"mozilla.org","contentType":"text/plain; charset=UTF-8"},
+{"type":"httpResponse","domain":"mozilla.org","referrerDomain":"mozilla.org","contentType":"text/plain; charset=UTF-8"},
+{"type":"httpResponse","domain":"mozilla.org","referrerDomain":"mozilla.org","contentType":"image/png"},
+{"type":"httpResponse","domain":"webtrendslive.com","referrerDomain":"mozilla.org","contentType":null},
+{"type":"httpResponse","domain":"webtrendslive.com","referrerDomain":"mozilla.org","contentType":"image/gif"},
+{"type":"formSubmit","data":{"authenticity_token":"Z6DhFKS6+2RbT/ilV3UHN2USmPgij1bAw/CV7oCeMNQ=","email":"vedad@personal.com","login_button":"Login","timezone":""}}
+]
+
 (function($){
 
   $(function(){
