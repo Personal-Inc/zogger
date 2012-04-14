68d8c3515cef5e1cfce0507254cd3ad277c3a929
vedadk <vedad@personal.com>
2012-04-14 15:02:14 -0400

    Dummy fieldFacets implementation

--- data/ui/report.js
+++ data/ui/report.js
@@ -10,6 +10,16 @@ var Zogger = (function(){
     console.log(reportData);
   }
 
+  function fieldNameFacet(){
+    return {
+      'first_name' : 3,
+      'last_name' : 3, 
+      'name' : 22,
+      'email' : 66,
+      'password' : 20
+    };
+  }
+
   return {
     printToConsole : printToConsole,
     updateReport : updateReport
