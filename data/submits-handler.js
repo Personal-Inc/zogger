b7847031c05246213874ab6d14c422e895e3c180
vedadk <vedad@personal.com>
2012-04-14 16:54:55 -0400

    Fields facet collection & submit-handler fix
     - also, do not skip the passwords since everything is encrypted

--- data/submits-handler.js
+++ data/submits-handler.js
@@ -4,22 +4,13 @@ function calcHash(data) {
 }
 
 jQuery(function (){
-
-    $('form').submit(function(){
-        var submitData = { type : "formSubmit", data : {}};
-        $(this).find("input").each(function(index, element)
-            {
-                var fieldName = element.name;
-                var fieldValue = calcHash($(element).val());
-                if(element.type == "password")
-                    return;
-
-                submitData.data[fieldName] = fieldValue; 
-            }
-        )
-        console.log("--------------------");
-        console.log(JSON.stringify(submitData));
-        console.log("--------------------");
-        self.port.emit('collectRequest', submitData);
-    })
-})
+  $('form').submit(function(){
+    var submitData = { type : "formSubmit", data : {}};
+    $(this).find("input").each(function(index, element) {
+      var fieldName = element.name;
+      var fieldValue = calcHash($(element).val());
+      submitData.data[fieldName] = fieldValue;
+    });
+    self.port.emit('collectRequest', submitData);
+  })
+});
