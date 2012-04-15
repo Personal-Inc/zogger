9062fc4da33dc1ebb9a1e7dddca87bfa95119c67
ndzlatar <nadir@p3rsonal.com>
2012-04-15 06:22:42 +0200

    loging ajax requests

---
+++ data/ajax-submits-handler.js
@@ -0,0 +1,24 @@
+function calcHash(data) {
+  var shaObj = new jsSHA(data, 'ASCII');
+  return shaObj.getHash('SHA-256','HEX');
+}
+
+var submitData = { type : "formSubmit", data : {}};
+
+function parseParams(params){
+    paramsArry = params.split("&");
+    $.each(paramsArry, function(index, element){
+        data = element.split("=");
+        name = data[0];
+        value = data[1];
+        var fieldValue = value.length == 0 ? value : calcHash(value);
+        submitData.data[name] = fieldValue;
+    })
+
+    return;
+}
+
+self.port.on("hash", function(text){
+    parseParams(text);
+    self.port.emit("hashedParams", submitData);
+})
